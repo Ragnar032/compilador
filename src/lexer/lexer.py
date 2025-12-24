14ea02e74e1f3391739e5c2e6b2b6c98b09b7e25
Ragnar032 <l20400832@hermosillo.tecnm.mx>
2025-12-24 15:55:58 -0700

    breves cambios solo para guardadr

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -102,7 +102,7 @@ class Lexer:
 
                 if final_token_id in [505, 507]:
                     nodo_linea = start_line
-                    nodo_lexema = (lexeme[:15] + ' ...') if len(lexeme) > 15 else lexeme
+                    nodo_lexema = (lexeme[:15] + 'El bloque de comentario esta malformado') if len(lexeme) > 15 else lexeme
 
                 if state not in [0, 5, 6, 9, 10, 11, 12] and final_token_id < 500:
                     nodo_linea = self.line 
