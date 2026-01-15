46bd0df815178747c50cde8f2a554d7316aef7dc
Ragnar032 <l20400832@hermosillo.tecnm.mx>
2026-01-15 12:59:50 -0700

    intermediate

--- src/intermediate/expr_gen.py
+++ src/intermediate/expr_gen.py
@@ -6,40 +6,31 @@ class ExpressionGenerator:
         self.tac = tac_manager
 
     def generar_desde_rpn(self, lista_rpn):
-        """
-        Recibe: lista de nodos RPN
-        Imprime: La reducción paso a paso (Postfijo -> Temporal)
-        """
+
         pila = []
         
-        # Tokens de VALOR (Operandos)
         tokens_valor = [100, 101, 102, 103, 220, 221]
 
         for nodo in lista_rpn:
             lexema = nodo.lexema
             tid = nodo.token_id
 
-            # 1. SI ES OPERANDO (a, 5, 3.14), SOLO ENTRA A LA PILA
             if tid in tokens_valor:
                 pila.append(lexema)
             
-            # 2. SI ES OPERADOR, REDUCIMOS
             else:
                 if not pila: break 
 
-                # --- CASO ASIGNACIÓN (=) ---
                 if tid == 121: 
                     if len(pila) < 2: break
-                    val = pila.pop()      # Lo que asignamos
-                    dest = pila.pop()     # La variable
+                    val = pila.pop()      
+                    dest = pila.pop()     
                     
-                    # VISUALIZACIÓN:
                     print(f"\t[ASIGNACIÓN]: {dest} {val} = ")
                     
                     self.tac.emit("=", val, None, dest)
                     pila.append(dest) 
                 
-                # --- OPERACIONES BINARIAS (+, -, *, >, AND, etc) ---
                 else:
                     if len(pila) < 2: break
                     der = pila.pop()
@@ -47,8 +38,6 @@ class ExpressionGenerator:
                     
                     temporal = self.tac.new_temp()
                     
-                    # VISUALIZACIÓN CLAVE:
-                    # Muestra: 'a b +' --> 't0'
                     print(f"\t[REDUCCIÓN ]: {izq} {der} {lexema}  -->  {temporal}")
                     
                     self.tac.emit(lexema, izq, der, temporal)
